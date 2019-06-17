c1c066e5ac31a0b1aa8a2ce4cd90acf3213c6989
Tillmann Weimer <tillmann1970@gmail.com>
2019-06-17 13:46:28 +0200

    created links

--- front-page.php
+++ front-page.php
@@ -2,11 +2,21 @@
 
 get_header();
 
+/**
+ * The template for displayng the frontpage
+ */
+
+
+
 //Hero
+?>
+ <?php get_template_part('global-templates/hero'); ?> 
 
-//USPS
+ <?php get_template_part('global-templates/usps'); ?>
+ 
+ <?php get_template_part('global-templates/bikerides'); ?>
 
-//cykelturer
 
-//footer
+
+<?php
 get_footer();
