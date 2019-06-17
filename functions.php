d045b0e21e1661020a0eb248177b77733dc8b475
Tillmann Weimer <tillmann1970@gmail.com>
2019-06-17 11:27:39 +0200

    included scripts and styles/ added weather widget area

--- functions.php
+++ functions.php
@@ -8,6 +8,37 @@ function ck_register_menues() {
 
 add_action('init','ck_register_menues');
 
+/**
+ * Register scripts and styles
+ */
+
+ function ck_register_scripts_and_styles() {
+    /**
+	 * Styles
+	 */
+	// Add Bootstrap CSS
+	wp_enqueue_style('bootstrap', 'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css', [], '4.3.1', 'all');
+	// Add Theme CSS
+	wp_enqueue_style('style', get_stylesheet_directory_uri() . '/style.css', ['bootstrap']);
+	// Add Theme Print CSS
+	wp_enqueue_style('print-style', get_stylesheet_directory_uri() . '/assets/css/print.css', ['bootstrap', 'style'], null, 'print');
+	/**
+	 * Scripts
+	 */
+	// Remove WordPress jQuery
+	wp_deregister_script('jquery');
+	// Add jQuery
+	wp_enqueue_script('jquery', 'https://code.jquery.com/jquery-3.3.1.slim.min.js', [], '3.3.1', true);
+	// Add popper.js
+	wp_enqueue_script('popper', 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js', ['jquery'], '1.14.7', true);
+	// Add bootstrap.js
+	wp_enqueue_script('bootstrap', 'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js', ['jquery', 'popper'], '4.3.1', true);
+
+	//Add script.js
+	wp_enqueue_script('script', get_stylesheet_directory_uri() . '/assets/js/script.js', ['jquery', 'popper', 'bootstrap'], false, true);
+ };
+
+ add_action('wp_enqueue_scripts', 'ck_register_scripts_and_styles');
 
 /**
  * Register theme widget area
@@ -24,9 +55,9 @@ add_action('init','ck_register_menues');
         'before_title'  => '<h1 class="widget-title">',
         'after_title'   => '</h1>', 
      ]);
-     //Page Sidebar Widget area
+     //Weather Sidebar Widget area
      register_sidebar([
-        'name'          => 'Page Sidebar',
+        'name'          => 'Weather Widget Sidebar',
         'id'            => 'page-sidebar',
         'before_widget' => '<aside id="%1s" class="widget %2$s">',
         'after_widget'  => '</aside>',
