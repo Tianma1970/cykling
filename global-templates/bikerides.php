851dacd8d286ed9318a321e40a9aba21759fa6aa
Tillmann Weimer <tillmann1970@gmail.com>
2019-06-24 16:34:55 +0200

    created file

--- global-templates/bikerides.php
+++ global-templates/bikerides.php
@@ -1,3 +1,43 @@
 <?php
 
-//links to the template part which shows my bikerides
+
+$ck_bikerides = new WP_Query([
+    'post_type'         => 'ck_bikerides',
+    'posts_per_page'    => -1,
+    'order_by'          => 'title',
+    'order'             => 'asc'
+]);
+
+    //do we get anu usps?
+
+if($ck_bikerides->have_posts()) {
+    //success
+    ?>
+    <div class="wrapper" id ="wrapper-ck-usps">
+    
+        <div class="container">
+        
+            <h1><?php _e('My bikerides', 'cycling'); ?></h1>
+
+            <div class="row">
+            
+                <!--Loop over the ck_usp items-->
+                <?php
+                while($ck_bikerides->have_posts()){
+                    $ck_bikerides->the_post();
+                    get_template_part('loop-templates/content', 'bikerides'); ?>
+
+                <?php
+                }
+                //don't forget to reset postdata
+                wp_reset_postdata();
+                ?>
+            
+            </div>
+        
+        </div>
+    
+    </div>
+<?php
+}
+
---
+++ loop-templates/content-bikerides.php
@@ -0,0 +1,15 @@
+<div class="col-md-4">
+
+    <div class="usp">
+    
+    <?php
+    //add support for icons later
+    ?>
+        <h1><?php the_title(); ?></h1>
+        <?php
+        the_excerpt();
+        ?>
+    
+    </div>
+
+</div>
