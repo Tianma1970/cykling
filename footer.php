422b404de74f799882093eace09e3c6ac76b613b
Tillmann Weimer <tillmann1970@gmail.com>
2019-06-24 16:27:25 +0200

    created file

---
+++ footer.php
@@ -0,0 +1,12 @@
+        <footer class="site-footer">
+            <div class="container">
+                <div class="row">
+                    <?php dynamic_sidebar('footer-sidebar'); ?>
+                </div>
+            </div>
+            <!--start wp_footer-->
+            <?php wp_footer(); ?>
+            <!--end wp_footer-->
+        </footer>
+    </body>
+</html>
