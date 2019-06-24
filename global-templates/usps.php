3e58a3739402f0e5eecdea0db041dfa6e5e6872f
Tillmann Weimer <tillmann1970@gmail.com>
2019-06-24 14:47:44 +0200

    created file

--- global-templates/usps.php
+++ global-templates/usps.php
@@ -1,4 +1,42 @@
 <?php
 
+$ck_usps = new WP_Query([
+    'post_type'         => 'ck_usp',
+    'posts_per_page'    => -1,
+    'order_by'          => 'title',
+    'order'             => 'asc'
+]);
 
+    //do we get anu usps?
+
+if($ck_usps->have_posts()) {
+    //success
+    ?>
+    <div class="wrapper" id ="wrapper-ck-usps">
+    
+        <div class="container">
+        
+            <h1><?php _e('Why travelling by cycle', 'cycling'); ?></h1>
+
+            <div class="row">
+            
+                <!--Loop over the ck_usp items-->
+                <?php
+                while($ck_usps->have_posts()){
+                    $ck_usps->the_post();
+                    get_template_part('loop-templates/content', 'usp'); ?>
+
+                <?php
+                }
+                //don't forget to reset postdata
+                wp_reset_postdata();
+                ?>
+            
+            </div>
+        
+        </div>
+    
+    </div>
+<?php
+}
 
