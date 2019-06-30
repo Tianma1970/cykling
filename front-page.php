4d805b6d5b7c347c836a476bb8e4e3d17a10f980
Tillmann Weimer <tillmann1970@gmail.com>
2019-06-30 13:17:09 +0200

    added bootstrap classes

--- front-page.php
+++ front-page.php
@@ -10,11 +10,20 @@ get_header();
 
 //Hero
 ?>
- <?php get_template_part('global-templates/hero'); ?> 
-
- <?php get_template_part('global-templates/usps'); ?>
+    <?php get_template_part('global-templates/hero'); ?> 
+<div class="row">
+    <div class="col-md-9">
+        <?php get_template_part('global-templates/usps'); ?>
  
- <?php get_template_part('global-templates/bikerides'); ?>
+        <?php get_template_part('global-templates/bikerides'); ?>
+
+    </div>
+
+    <div class="col-md-3">
+        <?php get_sidebar(); ?>
+    </div>
+</div>
+ 
 
 
 
