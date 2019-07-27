f390b9e38a19e0b459860b67ff8ee66504c993e8
Tillmann Weimer <tillmann1970@gmail.com>
2019-07-27 12:39:46 +0200

    changes

--- index.php
+++ index.php
@@ -1,3 +1,21 @@
 <?php
 
+get_header(); ?>
 
+<main class="container">
+    <section class="row">
+        <section class="row">
+            <div class="col-md-9">
+
+            </div>
+            <div class="col-md-3">
+                
+            </div>
+        </section>
+    </section>
+</main>
+
+
+
+
+<?php get_footer();
