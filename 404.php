4b7df5d4ebbfaef39a485b4e9add44ba1150af8b
Tillmann Weimer <tillmann1970@gmail.com>
2019-07-27 12:35:42 +0200

    created file

---
+++ 404.php
@@ -0,0 +1,5 @@
+<div class="error-wrapper"style="background-color:rgb(170, 170, 170">
+    <h1>something went wrong</h1>
+
+    <iframe width="1000" height="650" src="https://www.youtube.com/embed/VjEq-r2agqc?   controls=0&amp;start=7&autoplay=1" frameborder="0" allow="accelerometer;   autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></  iframe>
+</div>
