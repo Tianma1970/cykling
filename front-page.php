44b2396f7e244e53523dacc00f69208775067049
Tillmann Weimer <tillmann1970@gmail.com>
2019-08-04 12:24:02 +0200

    commented out slider

--- front-page.php
+++ front-page.php
@@ -11,16 +11,39 @@ get_header();
 //Hero
 ?>
     <?php get_template_part('global-templates/hero'); ?> 
-<div class="row">
-    <div class="col-md-9">
-        <?php get_template_part('global-templates/usps'); ?>
- 
-        <?php get_template_part('global-templates/bikerides'); ?>
+<div class="slider">
+    <div class="row">
+        <div class="col-md">
+            <?php get_template_part('global-templates/usps'); 
+            //The slider
+            ?>
 
-    </div>
+                <!-- <div class="row">
+                    <div class="col-md-9 mt-3">
+                            <h1 class="text-center">Some impressions</h1>
+                                <div id="slider">
+                                <ul class="slides mt-5">
+                                    <li class="slide"><img src="../images/Kavlinge.jpeg" height="400" width="680"></li>
+                                    <li class="slide"><img src="" height="400" width="680"></li>
+                                    <li class="slide"><img src="" height="400" width="680"></li>
+                                    <li class="slide"><img src="" height="400" width="680"></li>
+                                    <li class="slide"><img src="" height="400" width="680"></li>
+                                </ul>
 
-    <div class="col-md-3">
-        <?php get_sidebar(); ?>
+                            </div>
+                    </div>
+
+                    <div class="col-md-3 mt-3">
+                        <?php get_sidebar(); ?>
+                    </div>
+                </div> -->
+
+    
+            <?php get_template_part('global-templates/bikerides'); ?>
+
+        </div>
+
+
     </div>
 </div>
  
