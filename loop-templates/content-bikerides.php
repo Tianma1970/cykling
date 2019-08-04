16c3fadcf6cfcce86473d545ae1d4a66d3e73b46
Tillmann Weimer <tillmann1970@gmail.com>
2019-08-04 22:41:12 +0200

    added functionality for featured image

--- loop-templates/content-bikerides.php
+++ loop-templates/content-bikerides.php
@@ -1,15 +1,22 @@
-<div class="col-md-4">
+    <!-- <div class="row"> -->
+        <div class="col-md-4">
 
-    <div class="usp">
-    
-    <?php
-    //add support for icons later
-    ?>
-        <h1><?php the_title(); ?></h1>
-        <?php
-        the_excerpt();
-        ?>
-    
-    </div>
+            <div class="usp">
 
-</div>
+            <?php
+            //add support for featured images later
+            if(has_post_thumbnail()): ?>
+            <?php echo the_post_thumbnail( 'post-thumbnail', [
+                'class' => 'card-img-top img-fluid mt-3',
+            ]);
+            ?>
+            <?php endif; ?>
+                <h1><?php the_title(); ?></h1>
+                <?php
+                the_excerpt();
+                ?>
+
+            </div>
+
+        </div>
+    <!-- </div> -->
