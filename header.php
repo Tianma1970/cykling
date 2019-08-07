9aa10090096cfeb3e7e5fa2823b2034e024eb483
Tillmann Weimer <tillmann1970@gmail.com>
2019-08-07 18:59:54 +0200

    navbar bugfix

--- header.php
+++ header.php
@@ -9,14 +9,6 @@
 </head>
 <body <?php body_class(); ?>>
 
-<?php if (has_nav_menu('main-menu')) {
-    wp_nav_menu([
-        'theme_location' => 'main-menu',
-        //'container_class' => 'collapse navbar-collapse'
-        ]);
-                
-                } ?>
-
 <?php get_template_part('global-templates/header-image'); ?>
 
     
