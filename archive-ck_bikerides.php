a4e4afd0b76aa3a739b2cf96a9a9fa849bacfcb9
Tillmann Weimer <tillmann1970@gmail.com>
2019-08-07 18:53:46 +0200

    created file

---
+++ archive-ck_bikerides.php
@@ -0,0 +1,31 @@
+
+<?php get_header(); ?>
+
+<?php get_template_part('nav');
+
+if(have_posts()) {
+    //success
+}
+?>
+<div class="wrapper" id="wrapper-bikerides">
+    <div class="container">
+        <h1><?php __('All Bikerides', 'cykling'); ?></h1>
+            <div class="row">
+                <!--loop over the ck_bikerides items-->
+                <?php while(have_posts()) {
+                    the_post();
+                    get_template_part('loop-templates/content', 'bikerides'); 
+
+                    //don't forget to reset postdata
+                    //wp_reset_postdata();
+                }
+                ?>  
+            </div>
+    </div>
+</div>
+    
+
+
+
+<?php get_footer();
+
