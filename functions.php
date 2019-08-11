89bf2677364a72f0bd14fd26e55e984a4f5c6eb3
Tillmann Weimer <tillmann1970@gmail.com>
2019-08-11 22:05:55 +0200

    add hero image functionality

--- functions.php
+++ functions.php
@@ -54,6 +54,8 @@ add_action('init','ck_register_menues');
      set_post_thumbnail_size(180, 0, false);
      //Add Image size for single post featured image
      add_image_size('featured-image', 1110, 0, false);
+     //Add Image size for hero image featured image
+     add_image_size('hero_image', 1110, 500, false);
      //Add support for custom logo
      add_theme_support('custom-logo', [
                'height'       => 40,
@@ -131,10 +133,222 @@ function cykling_excerpt_more($more) {
 add_filter('excerpt_more', 'cykling_excerpt_more', 999, 1);
 
 /**
- * include font-awesome
+ * include cpt ui
  */
+function cptui_register_my_cpts() {
+
+	/**
+	 * Post Type: USPs.
+	 */
+
+	$labels = array(
+		"name" => __( "USPs", "cykling" ),
+		"singular_name" => __( "USP", "cykling" ),
+	);
+
+	$args = array(
+		"label" => __( "USPs", "cykling" ),
+		"labels" => $labels,
+		"description" => "",
+		"public" => true,
+		"publicly_queryable" => true,
+		"show_ui" => true,
+		"delete_with_user" => false,
+		"show_in_rest" => true,
+		"rest_base" => "",
+		"rest_controller_class" => "WP_REST_Posts_Controller",
+		"has_archive" => false,
+		"show_in_menu" => true,
+		"show_in_nav_menus" => true,
+		"exclude_from_search" => false,
+		"capability_type" => "post",
+		"map_meta_cap" => true,
+		"hierarchical" => false,
+		"rewrite" => array( "slug" => "ck_usp", "with_front" => true ),
+		"query_var" => true,
+		"menu_icon" => "dashicons-editor-ol",
+		"supports" => array( "title", "editor" ),
+	);
+
+	register_post_type( "ck_usp", $args );
+
+	/**
+	 * Post Type: Bikerides.
+	 */
+
+	$labels = array(
+		"name" => __( "Bikerides", "cykling" ),
+		"singular_name" => __( "Bikeride", "cykling" ),
+	);
+
+	$args = array(
+		"label" => __( "Bikerides", "cykling" ),
+		"labels" => $labels,
+		"description" => "",
+		"public" => true,
+		"publicly_queryable" => true,
+		"show_ui" => true,
+		"delete_with_user" => false,
+		"show_in_rest" => true,
+		"rest_base" => "",
+		"rest_controller_class" => "WP_REST_Posts_Controller",
+		"has_archive" => true,
+		"show_in_menu" => true,
+		"show_in_nav_menus" => true,
+		"exclude_from_search" => false,
+		"capability_type" => "post",
+		"map_meta_cap" => true,
+		"hierarchical" => true,
+		"rewrite" => array( "slug" => "ck_bikerides", "with_front" => true ),
+		"query_var" => true,
+		"menu_icon" => "dashicons-buddicons-replies",
+		"supports" => array( "title", "editor", "thumbnail", "excerpt", "custom-fields", "author" ),
+	);
+
+	register_post_type( "ck_bikerides", $args );
+}
+
+add_action( 'init', 'cptui_register_my_cpts' );
+
 
 add_action( 'wp_enqueue_scripts', 'enqueue_load_fa' );
 function enqueue_load_fa() {
 wp_enqueue_style( 'load-fa', 'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css' );
 }
+
+//establish path to plugins
+
+$ck_bikerides_plugins = array(
+   '/acf.php',
+   //including acf and its data
+   '/acf_cycle_data.php',
+   '/acf_hero_content.php',
+);
+
+foreach ( $ck_bikerides_plugins as $file ) {
+	$filepath = locate_template( 'inc' . $file );
+	if ( ! $filepath ) {
+		trigger_error( sprintf( 'Error locating /inc%s for inclusion', $file ), E_USER_ERROR );
+	}
+	require_once $filepath;
+};
+
+/*
+include cpt_ui_taxonomies
+*/
+function cptui_register_my_taxes() {
+
+	/**
+	 * Taxonomy: Locations.
+	 */
+
+	$labels = array(
+		"name" => __( "Locations", "cykling" ),
+		"singular_name" => __( "Location", "cykling" ),
+	);
+
+	$args = array(
+		"label" => __( "Locations", "cykling" ),
+		"labels" => $labels,
+		"public" => true,
+		"publicly_queryable" => true,
+		"hierarchical" => true,
+		"show_ui" => true,
+		"show_in_menu" => true,
+		"show_in_nav_menus" => true,
+		"query_var" => true,
+		"rewrite" => array( 'slug' => 'ck_bikeride_location', 'with_front' => true, ),
+		"show_admin_column" => false,
+		"show_in_rest" => true,
+		"rest_base" => "ck_bikeride_location",
+		"rest_controller_class" => "WP_REST_Terms_Controller",
+		"show_in_quick_edit" => false,
+		);
+	register_taxonomy( "ck_bikeride_location", array( "ck_bikerides" ), $args );
+
+	/**
+	 * Taxonomy: Countries.
+	 */
+
+	$labels = array(
+		"name" => __( "Countries", "cykling" ),
+		"singular_name" => __( "Country", "cykling" ),
+	);
+
+	$args = array(
+		"label" => __( "Countries", "cykling" ),
+		"labels" => $labels,
+		"public" => true,
+		"publicly_queryable" => true,
+		"hierarchical" => true,
+		"show_ui" => true,
+		"show_in_menu" => true,
+		"show_in_nav_menus" => true,
+		"query_var" => true,
+		"rewrite" => array( 'slug' => 'ck_bikeride_country', 'with_front' => true, ),
+		"show_admin_column" => false,
+		"show_in_rest" => true,
+		"rest_base" => "ck_bikeride_country",
+		"rest_controller_class" => "WP_REST_Terms_Controller",
+		"show_in_quick_edit" => false,
+		);
+	register_taxonomy( "ck_bikeride_country", array( "ck_bikerides" ), $args );
+
+	/**
+	 * Taxonomy: Categories.
+	 */
+
+	$labels = array(
+		"name" => __( "Categories", "cykling" ),
+		"singular_name" => __( "Category", "cykling" ),
+	);
+
+	$args = array(
+		"label" => __( "Categories", "cykling" ),
+		"labels" => $labels,
+		"public" => true,
+		"publicly_queryable" => true,
+		"hierarchical" => true,
+		"show_ui" => true,
+		"show_in_menu" => true,
+		"show_in_nav_menus" => true,
+		"query_var" => true,
+		"rewrite" => array( 'slug' => 'ck_bikeride_caregory', 'with_front' => true, ),
+		"show_admin_column" => false,
+		"show_in_rest" => true,
+		"rest_base" => "ck_bikeride_caregory",
+		"rest_controller_class" => "WP_REST_Terms_Controller",
+		"show_in_quick_edit" => false,
+		);
+	register_taxonomy( "ck_bikeride_caregory", array( "ck_bikerides" ), $args );
+
+	/**
+	 * Taxonomy: Years.
+	 */
+
+	$labels = array(
+		"name" => __( "Years", "cykling" ),
+		"singular_name" => __( "Year", "cykling" ),
+	);
+
+	$args = array(
+		"label" => __( "Years", "cykling" ),
+		"labels" => $labels,
+		"public" => true,
+		"publicly_queryable" => true,
+		"hierarchical" => true,
+		"show_ui" => true,
+		"show_in_menu" => true,
+		"show_in_nav_menus" => true,
+		"query_var" => true,
+		"rewrite" => array( 'slug' => 'ck_bikeride_year', 'with_front' => true, ),
+		"show_admin_column" => false,
+		"show_in_rest" => true,
+		"rest_base" => "ck_bikeride_year",
+		"rest_controller_class" => "WP_REST_Terms_Controller",
+		"show_in_quick_edit" => false,
+		);
+	register_taxonomy( "ck_bikeride_year", array( "ck_bikerides" ), $args );
+}
+add_action( 'init', 'cptui_register_my_taxes' );
+
