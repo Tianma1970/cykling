48a5afa3cb50052180186c65511605c95c4730cc
Tillmann Weimer <tillmann1970@gmail.com>
2019-08-11 22:13:06 +0200

    removed functionality for icons

--- loop-templates/content-usp.php
+++ loop-templates/content-usp.php
@@ -1,10 +1,6 @@
 <div class="col-md-4">
 
     <div class="usp">
-    
-    <?php
-    //add support for icons later
-    ?>
         <h1><?php the_title(); ?></h1>
         <?php
         the_content();
